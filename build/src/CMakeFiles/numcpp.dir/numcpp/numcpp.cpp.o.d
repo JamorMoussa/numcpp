f952364b9cf66b573a20b4de5ed93465834d43af
JamorMoussa <moussajamorsup@gmail.com>
2023-12-20 02:18:53 +0100

    remove the build folder

--- build/src/CMakeFiles/numcpp.dir/numcpp/numcpp.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/numcpp.dir/numcpp/numcpp.cpp.o: \
- /home/moussa/Documents/numcpp/src/numcpp/numcpp.cpp \
- /usr/include/stdc-predef.h \
- /home/moussa/Documents/numcpp/src/../include/numcpp/numcpp.h
